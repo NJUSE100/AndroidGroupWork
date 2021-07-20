c934e44838249590e280a1c792b3739bd61b578e
HP <191250100@smail.nju.edu.cn>
2021-07-21 01:00:28 +0800

    feat: Init

---
+++ Termwork/app/src/main/java/com/example/simpletito/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.simpletito;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+}
